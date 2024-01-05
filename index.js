c4f669d3eac866b6c2fe0175bedde145450d2d85
Phil Wekullo <philwekullo@gmail.com>
2024-01-06 00:50:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            Accept: 'application/json',
+        },
+        body:JSON.stringify({
+            name: userName,
+            email: userEmail
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+        const id = data.id;
+        // Appending the id to the DOM
+        const li = document.createElement('li');
+        li.textContent = `User with id ${id} was created.`;
+        document.body.appendChild(li);
+     })
+    .catch(error => {
+       // Appending the error message to the DOM
+       const li = document.createElement('li');
+       li.textContent = `Error: ${error.message}`;
+       document.body.appendChild(li);
+    });
+}
